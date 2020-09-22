1b1022d831fa04f08d798edacec22a39b23ebfaa
Victor Cazacov <cazacov.git@gmail.com>
2020-09-22 23:23:05 +0200

    Implement quick sort

--- challenge-01/main.c
+++ challenge-01/main.c
@@ -1,9 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define SWAP(x,y) do {   \
+   typeof(x) _x = x;      \
+   typeof(y) _y = y;      \
+   x = _y;                \
+   y = _x;                \
+ } while(0)
+
 void print_array(int array[static 1], size_t size);
+void init_random(int array[], size_t array_size);
 
 int *merge_sort(int *array, int *buffer, size_t left, size_t right);
+
+size_t partition(int array[], size_t low, size_t high);
+void quickSort(int array[], size_t low, size_t high);
 
 int main() {
 
@@ -12,19 +23,39 @@ int main() {
     size_t array_size = sizeof(array) / sizeof(array[0]);
 
     // Initialize array with some random data
-    for (size_t i = 0; i < array_size; i++) {
-        array[i] = rand() % 1000;
-    }
-
+    init_random(array, array_size);
     printf("Initial state:\n");
     print_array(array, array_size);
 
     int *result = merge_sort(array, buffer, 0, array_size - 1);
 
-    printf("Final state:\n");
+    printf("Sorted with merge sort:\n");
+    print_array(result, array_size);
+
+    init_random(array, array_size);
+    printf("Initial state:\n");
+    print_array(array, array_size);
+
+    quickSort(array, 0, array_size - 1);
+
+    printf("Sorted with quick sort:\n");
     print_array(result, array_size);
 
     return 0;
+}
+
+void quickSort(int array[], size_t low, size_t high) {
+    if (low < high)
+    {
+        /* pi is partitioning index, arr[p] is now
+           at right place */
+        size_t pi = partition(array, low, high);
+
+        // Separately sort elements before
+        // partition and after partition
+        quickSort(array, low, pi - 1);
+        quickSort(array, pi + 1, high);
+    }
 }
 
 // Merge sort: https://de.wikipedia.org/wiki/Mergesort
@@ -67,4 +98,32 @@ void print_array(int array[static 1], size_t size) {
         printf("%d\t", array[i]);
     }
     printf("\n");
+}
+
+#pragma clang diagnostic push
+#pragma ide diagnostic ignored "cert-msc50-cpp"
+// Initializes array with random values
+void init_random(int array[], size_t array_size) {
+    for (size_t i = 0; i < array_size; i++) {
+        array[i] = rand() % 1000;
+    }
+}
+#pragma clang diagnostic pop
+
+size_t partition(int *array, size_t low, size_t high) {
+    int pivot = array[high];    // pivot
+    ptrdiff_t  i = (low - 1);  // Index of smaller element
+
+    for (size_t j = low; j <= high - 1; j++)
+    {
+        // If current element is smaller than or
+        // equal to pivot
+        if (array[j] <= pivot)
+        {
+            i++;    // increment index of smaller element
+            SWAP(array[i], array[j]);
+        }
+    }
+    SWAP(array[i + 1], array[high]);
+    return (i + 1);
 }
